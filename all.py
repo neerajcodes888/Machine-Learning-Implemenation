c2bc958d5d4905721dbdc4d7a7306bfeb3808c9d
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2023-12-06 12:16:52 +0530

    Update all.py

--- all.py
+++ all.py
@@ -691,6 +691,120 @@ Q-1 Expectations Maximization
 
 
 
+------------PCA------
+import numpy as np
+
+def pca(X, num_components):
+    # Standardize the data
+    X_std = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+
+    # Compute the covariance matrix
+    cov_matrix = np.cov(X_std, rowvar=False)
+
+    # Compute the eigenvalues and eigenvectors
+    eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+
+    # Sort eigenvalues and corresponding eigenvectors in descending order
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    eigenvalues = eigenvalues[sorted_indices]
+    eigenvectors = eigenvectors[:, sorted_indices]
+
+    # Select the top 'num_components' eigenvectors
+    top_eigenvectors = eigenvectors[:, :num_components]
+
+    # Project the original data onto the new feature space
+    X_pca = np.dot(X_std, top_eigenvectors)
+
+    return X_pca
+
+# Example usage
+# Generate random data for demonstration
+np.random.seed(42)
+data = np.random.rand(100, 3)
+
+# Perform PCA with 2 components
+result = pca(data, num_components=2)
+
+print("Original data shape:", data.shape)
+print("Transformed data shape:", result.shape)
+
+
+
+----------EMA-----------------------
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+# Generate a dataset with two Gaussian components
+mu1, sigma1 = 2, 1
+mu2, sigma2 = -1, 0.8
+X1 = np.random.normal(mu1, sigma1, size=200)
+X2 = np.random.normal(mu2, sigma2, size=600)
+X = np.concatenate([X1, X2])
+
+# Plot the density estimation using seaborn
+sns.kdeplot(X)
+plt.xlabel('X')
+plt.ylabel('Density')
+plt.title('Density Estimation of X')
+plt.show()
+
+
+# Initialize parameters
+mu1_hat, sigma1_hat = np.mean(X1), np.std(X1)
+mu2_hat, sigma2_hat = np.mean(X2), np.std(X2)
+pi1_hat, pi2_hat = len(X1) / len(X), len(X2) / len(X)
+
+
+# Perform EM algorithm for 20 epochs
+num_epochs = 20
+log_likelihoods = []
+
+for epoch in range(num_epochs):
+	# E-step: Compute responsibilities
+	gamma1 = pi1_hat * norm.pdf(X, mu1_hat, sigma1_hat)
+	gamma2 = pi2_hat * norm.pdf(X, mu2_hat, sigma2_hat)
+	total = gamma1 + gamma2
+	gamma1 /= total
+	gamma2 /= total
+	
+	# M-step: Update parameters
+	mu1_hat = np.sum(gamma1 * X) / np.sum(gamma1)
+	mu2_hat = np.sum(gamma2 * X) / np.sum(gamma2)
+	sigma1_hat = np.sqrt(np.sum(gamma1 * (X - mu1_hat)**2) / np.sum(gamma1))
+	sigma2_hat = np.sqrt(np.sum(gamma2 * (X - mu2_hat)**2) / np.sum(gamma2))
+	pi1_hat = np.mean(gamma1)
+	pi2_hat = np.mean(gamma2)
+	
+	# Compute log-likelihood
+	log_likelihood = np.sum(np.log(pi1_hat * norm.pdf(X, mu1_hat, sigma1_hat)
+								+ pi2_hat * norm.pdf(X, mu2_hat, sigma2_hat)))
+	log_likelihoods.append(log_likelihood)
+
+# Plot log-likelihood values over epochs
+plt.plot(range(1, num_epochs+1), log_likelihoods)
+plt.xlabel('Epoch')
+plt.ylabel('Log-Likelihood')
+plt.title('Log-Likelihood vs. Epoch')
+plt.show()
+
+# Plot the final estimated density
+X_sorted = np.sort(X)
+density_estimation = pi1_hat*norm.pdf(X_sorted,
+										mu1_hat, 
+										sigma1_hat) + pi2_hat * norm.pdf(X_sorted,
+																		mu2_hat, 
+																		sigma2_hat)
+
+
+plt.plot(X_sorted, gaussian_kde(X_sorted)(X_sorted), color='green', linewidth=2)
+plt.plot(X_sorted, density_estimation, color='red', linewidth=2)
+plt.xlabel('X')
+plt.ylabel('Density')
+plt.title('Density Estimation of X')
+plt.legend(['Kernel Density Estimation','Mixture Density'])
+plt.show()
 
 
 
